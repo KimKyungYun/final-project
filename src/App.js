c3630fd80544ad1ef5bf85fe635ea76ccd9ba9d3
KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
2023-02-18 01:03:09 +0900

    apply hashRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import GoogleMap from './pages/GoogleMap';
 import Home from './pages/Home';
 function App() {
 	return (
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<HashRouter>
 			<Routes>
 				<Route path='/' element={<Home />}></Route>
 				<Route path='/map' element={<GoogleMap />}></Route>
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
