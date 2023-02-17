69cf2a16fcbcbad81f523190f1ddc21b5e4b9e3d
KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
2023-02-18 01:22:08 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
 	return (
 		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Routes>
-				{console.log(process.env.PUBLIC_URL)}
 				<Route path='/' element={<Home />}></Route>
 				<Route path='/map' element={<GoogleMap />}></Route>
 			</Routes>
